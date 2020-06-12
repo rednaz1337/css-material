2f1206655e572cc2988b689f8e04adeb94b99712
Heinrich Zander <heinrich.zander@datenkunst.de>
2020-06-12 22:16:46 +0200

    Fotogalerie hinzugefügt

--- material.js
+++ material.js
@@ -7,13 +7,20 @@ function switchDarkTheme(){
         style.setProperty("--color-background-card", "rgb(30, 30, 30)");
         style.setProperty("--color-text", "rgb(231, 231, 231)");
         style.setProperty("--shadow-card", "0px 1px 1.2px rgba(0, 0, 0, 0.5)");
+        style.setProperty("--color-background-nav", "rgb(30, 30, 30)");
     }else{
         style.setProperty("--color-background", "rgb(240, 240, 240)");
         style.setProperty("--color-background-card", "white");
         style.setProperty("--color-text", "black");
         style.setProperty("--shadow-card", "0px 1px 1.2px rgba(0, 0, 0, 0.5)");
+        style.setProperty("--color-background-nav", "rgb(226, 226, 226)");
     }
 }
 
+var prefersDark = window.matchMedia("(prefers-color-scheme: dark)")
+
+if(prefersDark.matches){
+    darkCheckbox.checked = true;
+}
 
 darkCheckbox.addEventListener("change", switchDarkTheme)
